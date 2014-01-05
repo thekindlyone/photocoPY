66b3bd4ee93160827de4e4091b912a34689d12a1
Aritra Das <dodo.dodder@gmail.com>
2014-01-06 05:09:29 +0530

    refactored

--- photoco.py
+++ photoco.py
@@ -1,36 +1,25 @@
 import os, time, sys, shutil
 
-def main():
-    dir=os.getcwd()
-    if len(sys.argv)>1:
-        dir=sys.argv[-1]
-    photos=filter(lambda x:os.path.splitext(x)[-1].lower() in ['.png','.jpg','.bmp','.jpeg'],os.listdir(dir))
+def copytodir(fn):
+    timeinfo= time.ctime(os.stat(fn).st_mtime).split(' ')
+    newdir= os.path.join(os.path.dirname(fn),timeinfo[1]+' '+timeinfo[-1])
+    rc=1
+    if not os.path.exists(newdir):
+        try:
+            os.makedirs(newdir)
+            rc=2
+        except:
+            rc = sys.exc_info()[1]
+    try: shutil.move(fn,newdir)
+    except: rc = sys.exc_info()[1]
+    if rc==1: print fn ,' copied to dir ',newdir
+    else:
+        if rc==2: print fn ,'copied to dir ',newdir,'(created now)'
+        else : print 'uh oh ',rc
+    return rc
     
-    def copytodir(fn,d):
-        rcode=1
-        if not os.path.exists(d):
-            try:
-                os.makedirs(d)
-                rcode=2
-            except:
-                rcode = sys.exc_info()[1]
-        try: shutil.move(fn,d)
-        except: rcode = sys.exc_info()[1]
-        return rcode
-    
-    for photo in photos:
-        photo=os.path.join(dir,photo)
-        (mode, ino, dev, nlink, uid, gid, size, atime, mtime, ctime) = os.stat(photo)
-        timeinfo= time.ctime(mtime).split(' ')
-        newdirectory= os.path.join(dir,timeinfo[1]+' '+timeinfo[-1])
-        rc=copytodir(photo,newdirectory)
-        if rc==1: print photo ,' copied to dir ',newdirectory
-        else:
-            if rc==2: print photo ,'copied to dir ',newdirectory,'(created now)'
-            else : print 'uh oh ',rc
-         
-    #print photos
-
-
 if __name__ == '__main__':
-  main()
+    dir=os.getcwd()
+    if len(sys.argv)>1: dir=sys.argv[-1]
+    photos=filter(lambda x:os.path.splitext(x)[-1].lower() in ['.png','.jpg','.bmp','.jpeg'],[os.path.join(dir,each) for each in os.listdir(dir)])
+    log=map(copytodir,photos)
